e6c3d958f2bfde7aa463eb84478be21c1fcf91a6
RukatM <66641751+RukatM@users.noreply.github.com>
2022-12-04 11:34:07 +0000

    day 4

--- 4.py
+++ 4.py
@@ -16,8 +16,11 @@ with open("input4.txt","r") as file:
         data_fixed.append(line)
     # print(data_fixed)
     sum = 0
+    sum2 = 0
     for data_line in data_fixed:
         if all(i in data_line[0] for i in data_line[1]) or all(i in data_line[1] for i in data_line[0]):
             sum += 1
+        if any(i in data_line[0] for i in data_line[1]):
+            sum2 += 1
 
-    print(sum)
+    print(sum,sum2)
